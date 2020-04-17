7582dfa60b65a0d193bdf5563a768dfe112221ca
Joel Graf <graf.joel03@gmail.com>
2020-04-17 21:23:25 +0200

    Version 1

---
+++ PingPong_404/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace PingPong_404
+{
+    partial class frmPingPong
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pnlSpiel = new System.Windows.Forms.Panel();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.picBall = new System.Windows.Forms.PictureBox();
+            this.tmrSpiel = new System.Windows.Forms.Timer(this.components);
+            this.pnlSpiel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picBall)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlSpiel
+            // 
+            this.pnlSpiel.BackColor = System.Drawing.Color.SeaGreen;
+            this.pnlSpiel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlSpiel.Controls.Add(this.picBall);
+            this.pnlSpiel.Location = new System.Drawing.Point(12, 12);
+            this.pnlSpiel.Name = "pnlSpiel";
+            this.pnlSpiel.Size = new System.Drawing.Size(541, 311);
+            this.pnlSpiel.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 367);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Spiel starten";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // picBall
+            // 
+            this.picBall.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.picBall.Location = new System.Drawing.Point(291, 145);
+            this.picBall.Name = "picBall";
+            this.picBall.Size = new System.Drawing.Size(25, 25);
+            this.picBall.TabIndex = 0;
+            this.picBall.TabStop = false;
+            // 
+            // tmrSpiel
+            // 
+            this.tmrSpiel.Interval = 20;
+            this.tmrSpiel.Tick += new System.EventHandler(this.tmrSpiel_Tick);
+            // 
+            // frmPingPong
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.pnlSpiel);
+            this.Name = "frmPingPong";
+            this.Text = "Ping-Pong Spiel";
+            this.pnlSpiel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picBall)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox picBall;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Panel pnlSpiel;
+        private System.Windows.Forms.Timer tmrSpiel;
+    }
+}
+
